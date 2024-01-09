7e1eefc21b8598faebd2d9c97cadc02a7a72257c
Dmitry Spassky <toffguy77@gmail.com>
2024-01-09 15:47:21 +0300

    add liveness probe

--- cmd/main.go
+++ cmd/main.go
@@ -12,6 +12,7 @@ import (
 
 const (
 	PORT = "8080"
+	cnt  = 0
 )
 
 func getHost(w http.ResponseWriter, r *http.Request) {
@@ -28,8 +29,17 @@ func getTime() string {
 	return fmt.Sprint(t.Format("2006-01-02 15:04:05"))
 }
 
+func checkLiveness(w http.ResponseWriter, r *http.Request) {
+	if cnt%5 == 0 {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf("liveness check failed...")
+	}
+	io.WriteString(w, "Ok")
+}
+
 func main() {
 	http.HandleFunc("/", getHost)
+	http.HandleFunc("/liveness", checkLiveness)
 
 	fmt.Printf("start lintening on port http://127.0.0.1:%s\n", PORT)
 	err := http.ListenAndServe(":"+PORT, nil)
